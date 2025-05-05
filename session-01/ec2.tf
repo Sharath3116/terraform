338a73a304d9e25164238795734109d52d5ceb2b
Sharath3116 <sharath3192@gmail.com>
2025-05-05 17:16:53 +0530

    teraform

--- session-01/ec2.tf
+++ session-01/ec2.tf
@@ -1,8 +1,35 @@
 resource "aws_instance" "web" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.roboshop-all.id ]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_security_group" "roboshop-all" {
+  name        = var.sg_name
+  description = var.sg_description
+  #vpc_id      = "${aws_vpc.main.id}"
+  
+  ingress {
+    description = "All all ports"
+    from_port   = var.inbound_traffic
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+    from_port   = var.outbound_traffic
+    to_port     = 0
+    protocol    = 0
+    cidr_blocks = var.cidr_blocks
+  }
+
+  tags = {
+    Name = "roboshop-all"
+  }
+}
+
---
+++ session-01/variables.tf
@@ -0,0 +1,26 @@
+variable "sg_name" {
+  type    = string
+  default = "roboshop-all-aws"
+}
+
+variable "sg_description" {
+  type    = string
+  default = "Allow TLS inbound traffic"
+}
+
+variable "inbound_traffic" {
+  #type    = number
+  default = 0
+  # 443 is standard for TLS/HTTPS. Use 22 for SSH, 80 for HTTP, etc.
+}
+
+variable "outbound_traffic" {
+  #type    = number
+  default = -1
+  # You can safely use 0 here if you're allowing all traffic (protocol = "-1")
+}
+
+variable "cidr_blocks" {
+  #type    = list(string)
+  default = ["0.0.0.0/0"]
+}
