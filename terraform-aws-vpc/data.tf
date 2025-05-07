3ae8cc7e28abe2e33fb948a5a6b4bc9b393108e1
Sharath3116 <sharath3192@gmail.com>
2025-05-07 16:19:01 +0530

    script update

--- terraform-aws-vpc/data.tf
+++ terraform-aws-vpc/data.tf
@@ -3,3 +3,16 @@ data "aws_availability_zones" "azs" {
     #all_availability_zones = true
     
 }
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_route_table" "default" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+      name = "association.main"
+      values = ["true"]
+    }
+  
+}
